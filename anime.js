b431eae40c0a06b71f49b42f238524551c145c6a
tanm-sys <66236112+tanm-sys@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Update anime.js

--- anime.js
+++ anime.js
@@ -5,27 +5,29 @@ function addImageHoverAnimation() {
       img.addEventListener('mouseenter', () => {
         anime({
           targets: img,
-          scale: 1.2, 
-          rotate: '1turn', 
-          opacity: 0.8, 
-          duration: 500, 
-          easing: 'easeInOutQuart', 
+          scale: 1.2, // Increase the image size by 20% on hover
+          opacity: 0.8, // Reduce opacity to 80% on hover
+          translateX: 10, // Move the image 10 pixels to the right on hover
+          translateY: -10, // Move the image 10 pixels up on hover
+          duration: 500, // Animation duration in milliseconds (0.5 seconds)
+          easing: 'easeInOutQuart', // Smooth easing function for more dynamic animation
         });
       });
 
       img.addEventListener('mouseleave', () => {
         anime({
           targets: img,
-          scale: 1, 
-          rotate: 0, 
-          opacity: 1, 
-          duration: 500, 
-          easing: 'easeInOutQuart', 
+          scale: 1, // Reset the image size on mouse leave
+          opacity: 1, // Reset opacity to 100% on mouse leave
+          translateX: 0, // Reset the translation on mouse leave
+          translateY: 0, // Reset the translation on mouse leave
+          duration: 500, // Animation duration in milliseconds (0.5 seconds)
+          easing: 'easeInOutQuart', // Smooth easing function for more dynamic animation
         });
       });
     });
   }
 
-
+  // Call the function to add hover animation after the page loads
   document.addEventListener('DOMContentLoaded', addImageHoverAnimation);
 
