7d3e3c4abd02acb81bdbcda3e1741b8c183f1b42
tanm-sys <66236112+tanm-sys@users.noreply.github.com>
2023-08-05 04:36:51 +0000

    Create JS disabled.js

---
+++ JS disabled.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+      var jsDisabledMessage = document.getElementById("js-disabled-message");
+      jsDisabledMessage.style.display = "block"
+        anime({
+        targets: jsDisabledMessage,
+        opacity: [0, 1],
+        translateY: [-20, 0],
+        duration: 1000,
+        easing: 'easeInOutSine'
+      });
+    });
