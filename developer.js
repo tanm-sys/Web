ef9c32d85b90e17383bd05c730bb06f3a14a41bc
tanm-sys <66236112+tanm-sys@users.noreply.github.com>
2023-08-05 05:59:58 +0000

    Update developer.js

--- developer.js
+++ developer.js
@@ -1,1 +1,44 @@
-document.addEventListener("contextmenu",function(t){t.preventDefault(),showAnimatedAlert("Right-clicking is disabled on this website.")});function showAnimatedAlert(t){let e=document.createElement("div");e.textContent=t,e.style.position="fixed",e.style.top="50%",e.style.left="50%",e.style.transform="translate(-50%, -50%)",e.style.padding="15px 20px",e.style.border="1px solid #ccc",e.style.borderRadius="6px",e.style.backgroundColor="#f0f0f0",e.style.boxShadow="0 4px 8px rgba(0, 0, 0, 0.1)",e.style.zIndex="9999",e.style.opacity="0",document.body.appendChild(e),anime({targets:e,opacity:1,duration:500,easing:"easeInOutQuad"}),setTimeout(function(){anime({targets:e,opacity:0,duration:500,easing:"easeInOutQuad",complete:function(){e.remove()}})},3e3)}
+ // Disable right-click on the entire document
+  document.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+    showAnimatedAlert('Right-clicking is disabled on this website.');
+  });
+
+  function showAnimatedAlert(message) {
+    const alertBox = document.createElement('div');
+    alertBox.textContent = message;
+    alertBox.style.position = 'fixed';
+    alertBox.style.top = '50%';
+    alertBox.style.left = '50%';
+    alertBox.style.transform = 'translate(-50%, -50%)';
+    alertBox.style.padding = '15px 20px';
+    alertBox.style.border = '1px solid #ccc';
+    alertBox.style.borderRadius = '6px';
+    alertBox.style.backgroundColor = '#f0f0f0';
+    alertBox.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
+    alertBox.style.zIndex = '9999';
+    alertBox.style.opacity = '0';
+
+    document.body.appendChild(alertBox);
+
+    // Animate the alert box using anime.js
+    anime({
+      targets: alertBox,
+      opacity: 1,
+      duration: 500,
+      easing: 'easeInOutQuad',
+    });
+
+    // Hide the alert box after a delay
+    setTimeout(function () {
+      anime({
+        targets: alertBox,
+        opacity: 0,
+        duration: 500,
+        easing: 'easeInOutQuad',
+        complete: function () {
+          alertBox.remove();
+        },
+      });
+    }, 3000); // Display the alert for 3 seconds
+  }
