4053a883dc875bd987878bd122bfb1668e36416c
tanm-sys <66236112+tanm-sys@users.noreply.github.com>
2023-08-05 06:07:42 +0000

    Update developer.js

--- developer.js
+++ developer.js
@@ -1,9 +1,17 @@
-document.addEventListener('contextmenu', function (event) {
+ document.addEventListener('contextmenu', function (event) {
     event.preventDefault();
     showAnimatedAlert('Right-clicking is disabled on this website.');
   });
 
+  function getComputedStyleProp(el, propName) {
+    return window.getComputedStyle(el, null).getPropertyValue(propName);
+  }
+
   function showAnimatedAlert(message) {
+    const bodyBackgroundColor = getComputedStyleProp(document.body, 'background-color');
+    const bodyFontColor = getComputedStyleProp(document.body, 'color');
+    const bodyBoxShadow = getComputedStyleProp(document.body, 'box-shadow');
+
     const alertBox = document.createElement('div');
     alertBox.textContent = message;
     alertBox.style.position = 'fixed';
@@ -13,31 +21,83 @@ document.addEventListener('contextmenu', function (event) {
     alertBox.style.padding = '15px 20px';
     alertBox.style.border = '1px solid #ccc';
     alertBox.style.borderRadius = '6px';
-    alertBox.style.backgroundColor = '#f0f0f0';
-    alertBox.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
+    alertBox.style.backgroundColor = bodyBackgroundColor;
+    alertBox.style.color = bodyFontColor;
+    alertBox.style.boxShadow = bodyBoxShadow;
     alertBox.style.zIndex = '9999';
     alertBox.style.opacity = '0';
 
     document.body.appendChild(alertBox);
 
-   
-    anime({
+    // Animate the alert box using anime.js
+    anime.timeline({
       targets: alertBox,
-      opacity: 1,
-      duration: 500,
       easing: 'easeInOutQuad',
+      duration: 1000,
+    })
+    .add({
+      opacity: 1,
+      scale: [1, 1.2],
+    })
+    .add({
+      scale: 1,
+    })
+    .add({
+      rotate: 360,
+      duration: 800,
+    })
+    .add({
+      translateY: ['-50%', '-55%', '-50%'],
+      duration: 600,
+    })
+    .add({
+      translateY: ['-50%', '-45%', '-50%'],
+    })
+    .add({
+      translateY: ['-45%', '-50%', '-45%'],
+    })
+    .add({
+      translateY: ['-50%', '-55%', '-50%'],
+      scale: 1.1,
+    })
+    .add({
+      translateY: ['-55%', '-50%', '-55%'],
+      scale: 1,
+    })
+    .add({
+      translateY: ['-50%', '-45%', '-50%'],
+      scale: 1.1,
+    })
+    .add({
+      translateY: ['-45%', '-50%', '-45%'],
+      scale: 1,
+    })
+    .add({
+      translateY: ['-50%', '-52%', '-50%'],
+      scale: 1.03,
+    })
+    .add({
+      translateY: ['-52%', '-50%', '-52%'],
+      scale: 1,
+    })
+    .add({
+      translateY: ['-50%', '-51%', '-50%'],
+      scale: 1.01,
+      rotate: 5,
+    })
+    .add({
+      translateY: ['-51%', '-50%', '-51%'],
+      scale: 1,
+      rotate: 0,
+    })
+    .add({
+      opacity: 0,
+      translateY: ['-50%', '-50%', '-55%'],
+      scale: 1,
+      rotate: 180,
+      duration: 500,
+      complete: function() {
+        alertBox.remove();
+      },
     });
-
-  
-    setTimeout(function () {
-      anime({
-        targets: alertBox,
-        opacity: 0,
-        duration: 500,
-        easing: 'easeInOutQuad',
-        complete: function () {
-          alertBox.remove();
-        },
-      });
-    }, 3000); // Display the alert for 3 seconds
   }
