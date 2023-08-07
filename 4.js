3704c6022bb601d31a049381ca2b53b5c2b7eb4a
tanm-sys <66236112+tanm-sys@users.noreply.github.com>
2023-08-07 11:57:43 +0000

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,76 +1,32 @@
-   function isXMLHttpRequestLevel2Supported() {
-      return 'XMLHttpRequest' in window && 'withCredentials' in new XMLHttpRequest();
-    }
-
-    function addXFrameOptionsHeaderXMLHttpRequest2(url) {
-      $.ajax({
-        type: "HEAD",
-        url: url,
-        success: function (data, textStatus, request) {
-          var responseHeaders = request.getAllResponseHeaders();
-          if (responseHeaders.indexOf("X-Frame-Options") === -1) {
-            $.ajax({
-              type: "POST",
-              url: url,
-              beforeSend: function (xhr) {
-                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                xhr.setRequestHeader("X-Frame-Options", "SAMEORIGIN");
-              },
-              success: function () {
-                console.log("X-Frame-Options header added successfully.");
-              },
-              error: function (xhr, textStatus, errorThrown) {
-                console.log("Error adding X-Frame-Options header:", errorThrown);
-              }
-            });
-          }
-        },
-        error: function (xhr, textStatus, errorThrown) {
-          console.log("Error fetching headers:", errorThrown);
-        }
-      });
-    }
-
-    function addXFrameOptionsHeaderXMLHttpRequest1(url) {
-      $.ajax({
-        type: "HEAD",
-        url: url,
-        beforeSend: function (xhr) {
-          xhr.setRequestHeader("User-Agent", "XMLHTTP/1.0");
-        },
-        success: function (data, textStatus, request) {
-          var responseHeaders = request.getAllResponseHeaders();
-          if (responseHeaders.indexOf("X-Frame-Options") === -1) {
-            $.ajax({
-              type: "POST",
-              url: url,
-              beforeSend: function (xhr) {
-                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                xhr.setRequestHeader("X-Frame-Options", "SAMEORIGIN");
-              },
-              success: function () {
-                console.log("X-Frame-Options header added successfully.");
-              },
-              error: function (xhr, textStatus, errorThrown) {
-                console.log("Error adding X-Frame-Options header:", errorThrown);
-              }
-            });
-          }
-        },
-        error: function (xhr, textStatus, errorThrown) {
-          console.log("Error fetching headers:", errorThrown);
-        }
-      });
-    }
-
     function addXFrameOptionsHeader(url) {
-      if (isXMLHttpRequestLevel2Supported()) {
-        addXFrameOptionsHeaderXMLHttpRequest2(url);
-      } else {
-        addXFrameOptionsHeaderXMLHttpRequest1(url);
-      }
+      fetch(url, {
+        method: 'HEAD',
+        mode: 'no-cors'
+      })
+      .then(response => {
+        var responseHeaders = response.headers;
+        if (!responseHeaders.has("X-Frame-Options")) {
+          fetch(url, {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'X-Frame-Options': 'SAMEORIGIN'
+            }
+          })
+          .then(() => {
+            console.log("X-Frame-Options header added successfully.");
+          })
+          .catch(error => {
+            console.log("Error adding X-Frame-Options header:", error);
+          });
+        }
+      })
+      .catch(error => {
+        console.log("Error fetching headers:", error);
+      });
     }
 
-    addXFrameOptionsHeader("http://anonhacker99.blogspot.com/"); 
-
+    // Call the function to add X-Frame-Options header
+    addXFrameOptionsHeader("https://anonhacker99.blogspot.com/"); 
 
