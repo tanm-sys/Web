564019458b638ef208b8017cce0d486527c2fa68
tanm-sys <66236112+tanm-sys@users.noreply.github.com>
2023-09-06 04:40:38 +0000

    Update loading2.js

--- loading2.js
+++ loading2.js
@@ -1,15 +1,1 @@
-$(document).ready(function() {
-  $(window).on("load", function() {
-    // Simulate some asynchronous process (e.g., loading content)
-    setTimeout(function() {
-      $(".loader").fadeOut("slow", function() {
-        $(this).remove(); // Remove the loader element after fading out
-      });
-
-      // You can also reveal your content after the loader fades out
-      $("body").css("overflow", "visible"); // Restore scrolling
-      $(".content").fadeIn("slow"); // Replace ".content" with your content selector
-    }, 2000); // Change the delay time as needed
-  });
-});
-
+$(document).ready(function(){$(window).on("load",function(){setTimeout(function(){$(".loader").fadeOut("slow",function(){$(this).remove()}),$("body").css("overflow","visible"),$(".content").fadeIn("slow")},2e3)})});
